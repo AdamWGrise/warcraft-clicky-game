e62b49c4edb3b8b8e7cab98416280f596998b510
Adam Grise <adamwgrise@gmail.com>
2019-08-24 00:08:48 -0500

    Updates

---
+++ precache-manifest.c05a74adadc3e7588602e6177a36bd8c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db170ef58cc377133d37fb3575bbca4",
+    "url": "/warcraft-clicky-game/index.html"
+  },
+  {
+    "revision": "e6421bf03e9dce1e9fb3",
+    "url": "/warcraft-clicky-game/static/css/main.d3bfa71e.chunk.css"
+  },
+  {
+    "revision": "2f525d0e7b281d7e1407",
+    "url": "/warcraft-clicky-game/static/js/2.02a43346.chunk.js"
+  },
+  {
+    "revision": "e6421bf03e9dce1e9fb3",
+    "url": "/warcraft-clicky-game/static/js/main.d293501f.chunk.js"
+  },
+  {
+    "revision": "64381fba9e7cb40da68d",
+    "url": "/warcraft-clicky-game/static/js/runtime~main.886d4a32.js"
+  },
+  {
+    "revision": "784d039024036d66598d0bf9dc4723ca",
+    "url": "/warcraft-clicky-game/static/media/img01.784d0390.png"
+  },
+  {
+    "revision": "9d90b427708d57e0e25adfd901469f73",
+    "url": "/warcraft-clicky-game/static/media/img02.9d90b427.png"
+  },
+  {
+    "revision": "d35b8f38895d668f404c2b0e52cbf3f8",
+    "url": "/warcraft-clicky-game/static/media/img03.d35b8f38.png"
+  },
+  {
+    "revision": "fb02f09f2c0124838e8e34a078ebd7e6",
+    "url": "/warcraft-clicky-game/static/media/img04.fb02f09f.png"
+  },
+  {
+    "revision": "8c4d3bdc851d0c81970d70dcfa4058e1",
+    "url": "/warcraft-clicky-game/static/media/img05.8c4d3bdc.png"
+  },
+  {
+    "revision": "bce98df7b7535f3688c62054ae766518",
+    "url": "/warcraft-clicky-game/static/media/img06.bce98df7.png"
+  },
+  {
+    "revision": "b43c52fd9434d1aba619452471dd6656",
+    "url": "/warcraft-clicky-game/static/media/img07.b43c52fd.png"
+  },
+  {
+    "revision": "081dfebc6c450225979128c6f15463b4",
+    "url": "/warcraft-clicky-game/static/media/img08.081dfebc.png"
+  },
+  {
+    "revision": "333aa586e46f018e2999e4a592447a5a",
+    "url": "/warcraft-clicky-game/static/media/img09.333aa586.png"
+  },
+  {
+    "revision": "e94480118d7b8c0ad886ebdb11c71681",
+    "url": "/warcraft-clicky-game/static/media/img10.e9448011.png"
+  },
+  {
+    "revision": "88b78288b7c30645e3621d5d3522e4cc",
+    "url": "/warcraft-clicky-game/static/media/img11.88b78288.png"
+  },
+  {
+    "revision": "3756a2c8f360abca440558b3706feb50",
+    "url": "/warcraft-clicky-game/static/media/img12.3756a2c8.png"
+  },
+  {
+    "revision": "52474bd3c378a65a72463fa1f12c9937",
+    "url": "/warcraft-clicky-game/static/media/img13.52474bd3.png"
+  },
+  {
+    "revision": "30f8ebec73dd64b15a8b96b04a90b42d",
+    "url": "/warcraft-clicky-game/static/media/img14.30f8ebec.png"
+  }
+]);
