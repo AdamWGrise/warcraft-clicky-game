2b370df3b651cd74b9efe9891409457abbc21da2
Adam Grise <adamwgrise@gmail.com>
2019-08-25 20:06:28 -0500

    Revert "Updates"
    This reverts commit f9e87eb57d010f4e5a3b423d6087460823c33ebf.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./components/Game"
+import "./App.css";
+import "./components/Char.css"
+
+ReactDOM.render(<Game />, document.getElementById("root"));
